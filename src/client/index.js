c2db315b7eede2c0301964dda2da17a474c6e05b
King Tine <tine2musi1@gmail.com>
2025-07-25 08:06:10 +0200

    fix: Correct import statement for App component in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.js';
 import './styles/index.css';
 
 // Error boundary for the entire app
